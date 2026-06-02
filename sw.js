e7b8dbf75979e9ba2285e03f5f12b268448bd6c4
isaiahgrafas <isaiahgrafas@gmail.com>
2026-06-03 07:04:28 +1200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lockin-v2";
+const CACHE = "lockin-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
